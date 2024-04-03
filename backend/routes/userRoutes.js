87cdfca922ca26cd793c34b166be5b640875f5c8
Eric Mergelas <emergelas@gmail.com>
2024-04-03 12:08:13 -0400

    npm install in backend gave these

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User.js'); // Adjust the path as necessary
 const jwt = require('jsonwebtoken');
-const auth = require('userAuthRoute');
+const auth = require('./userAuthRoute');
 
 // POST route to create a new user
 router.post('/users', async (req, res) => {
