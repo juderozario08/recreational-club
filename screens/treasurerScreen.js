ce87cac7c8e273cab294dba43380e6080a6f6b5a
Jude Rozario <juderozario@judesMac.local>
2024-04-04 19:37:25 -0400

    Updated Treasurer Fetching

--- screens/treasurerScreen.js
+++ screens/treasurerScreen.js
@@ -1,156 +1,183 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, ScrollView } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { ScrollView, StyleSheet, Text } from "react-native";
 import uri from "../config/apiConfig";
-import { userid } from './Login';
+import { userid } from "./Login";
 
 const Drawer = createDrawerNavigator();
 
 // Income Statement Component
 const IncomeStatement = () => {
-    const [income, setIncome] = useState(0);
-    const [expenses, setExpenses] = useState(0);
+  const [income, setIncome] = useState(0);
+  const [expenses, setExpenses] = useState(0);
 
-    return (
-        <ScrollView contentContainerStyle={styles.container}>
-            <Text style={styles.heading}>Income Statement</Text>
-            <Text style={styles.text}>Revenue: {income}</Text>
-            <Text style={styles.text}>Expenses: {expenses}</Text>
-            <Text style={[styles.text, styles.profit]}>Profit: {income - expenses}</Text>
-        </ScrollView>
-    );
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.heading}>Income Statement</Text>
+      <Text style={styles.text}>Revenue: {income}</Text>
+      <Text style={styles.text}>Expenses: {expenses}</Text>
+      <Text style={[styles.text, styles.profit]}>
+        Profit: {income - expenses}
+      </Text>
+    </ScrollView>
+  );
 };
 
 // Unpaid Debts Component
 const UnpaidDebts = () => {
-    const [unpaidCoachExpenses, setUnpaidCoachExpenses] = useState();
-    const [unpaidHallExpenses, setUnpaidHallExpenses] = useState(300);
+  const [unpaidCoachExpenses, setUnpaidCoachExpenses] = useState();
+  const [unpaidHallExpenses, setUnpaidHallExpenses] = useState(300);
 
-    return (
-        <ScrollView contentContainerStyle={styles.container}>
-            <Text style={styles.heading}>Unpaid Debts</Text>
-            <Text style={styles.text}>Unpaid Coach Expenses: {unpaidCoachExpenses}</Text>
-            <Text style={styles.text}>Unpaid Hall Expenses: {unpaidHallExpenses}</Text>
-        </ScrollView>
-    );
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.heading}>Unpaid Debts</Text>
+      <Text style={styles.text}>
+        Unpaid Coach Expenses: {unpaidCoachExpenses}
+      </Text>
+      <Text style={styles.text}>
+        Unpaid Hall Expenses: {unpaidHallExpenses}
+      </Text>
+    </ScrollView>
+  );
 };
 
 // Account Payables Component
 const AccountPayables = () => {
-    const [accountPayables, setAccountPayables] = useState(400);
+  const [accountPayables, setAccountPayables] = useState(400);
 
-    return (
-        <ScrollView contentContainerStyle={styles.container}>
-            <Text style={styles.heading}>Account Payables</Text>
-            <Text style={styles.text}>Current Month's Account Payables: {accountPayables}</Text>
-        </ScrollView>
-    );
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.heading}>Account Payables</Text>
+      <Text style={styles.text}>
+        Current Month's Account Payables: {accountPayables}
+      </Text>
+    </ScrollView>
+  );
 };
 
 // Coach Management Component
 const CoachManagement = () => {
-    const [coachList, setCoachList] = useState([])
-    useEffect(() => {
-        const fetchCoaches = async () => {
-            try{
-                const response = await axios.get(`${uri}/users`)
-                console.log(response)
-                setCoachList(response.data)
-            }catch(e){
-                console.error('Error fetching coaches: ',e.message)
-            }
-        }
-        fetchCoaches()
-    }, []);
+  const [coachList, setCoachList] = useState([]);
+  useEffect(() => {
+    const fetchCoaches = async () => {
+      try {
+        const response = await axios.get(`${uri}/users/coaches`);
+        console.log(response);
+        setCoachList(response.data);
+      } catch (e) {
+        console.error("Error fetching coaches: ", e.message);
+      }
+    };
+    fetchCoaches();
+  }, []);
 
-    return (
-        <ScrollView contentContainerStyle={styles.container}>
-            <Text style={styles.heading}>Coach Management</Text>
-            {/*{coachList.map((coach, index) => (*/}
-            {/*    <Text key={index} style={styles.text}>{coach.name}</Text>*/}
-            {/*))}*/}
-        </ScrollView>
-    );
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.heading}>Coach Management</Text>
+      {coachList.map((coach, index) => (
+        <Text key={index} style={styles.text}>
+          {coach.name}
+        </Text>
+      ))}
+    </ScrollView>
+  );
 };
 
 // Member Management Component
 const MemberManagement = () => {
-    const [memberList, setMemberList] = useState(['Member 1', 'Member 2', 'Member 3']);
+  const [memberList, setMemberList] = useState([]);
+  useEffect(() => {
+    const fetchMembers = async () => {
+      try {
+        const response = await axios.get(`${uri}/users/members`);
+        console.log(response);
+        setMemberList(response.data);
+      } catch (e) {
+        console.error("Error fetching coaches: ", e.message);
+      }
+    };
+    fetchMembers();
+  }, []);
 
-    return (
-        <ScrollView contentContainerStyle={styles.container}>
-            <Text style={styles.heading}>Member Management</Text>
-            {memberList.map((member, index) => (
-                <Text key={index} style={styles.text}>{member}</Text>
-            ))}
-        </ScrollView>
-    );
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.heading}>Member Management</Text>
+      {memberList.map((member, index) => (
+        <Text key={index} style={styles.text}>
+          {member.name}
+        </Text>
+      ))}
+    </ScrollView>
+  );
 };
 
 const Profile = () => {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        const fetchUserData = async () => {
-            try {
-                const response = await axios.get(`${uri}/users/${userid}`);
-                setUser(response.data);
-            } catch (error) {
-                console.error('Error fetching user data:', error.message);
-                setUser(null);
-            }
-        };
-        fetchUserData().then(r => console.log('Fetched Treasurer Data'));
-    }, [userid]);
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get(`${uri}/users/${userid}`);
+        setUser(response.data);
+      } catch (error) {
+        console.error("Error fetching user data:", error.message);
+        setUser(null);
+      }
+    };
+    fetchUserData().then((r) => console.log("Fetched Treasurer Data"));
+  }, [userid]);
 
-    return (
-        <ScrollView contentContainerStyle={styles.container}>
-            {user ? (
-                <>
-                    <Text style={styles.heading}>Profile</Text>
-                    <Text style={styles.text}>Name: {user.name}</Text>
-                    <Text style={styles.text}>Email: {user.email}</Text>
-                    <Text style={styles.text}>Address: {user.address}</Text>
-                    <Text style={styles.text}>Phone Number: {user.phoneNumber}</Text>
-                </>
-            ) : (
-                <Text style={styles.text}>Loading user data...</Text>
-            )}
-        </ScrollView>
-    );
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      {user ? (
+        <>
+          <Text style={styles.heading}>Profile</Text>
+          <Text style={styles.text}>Name: {user.name}</Text>
+          <Text style={styles.text}>Email: {user.email}</Text>
+          <Text style={styles.text}>Address: {user.address}</Text>
+          <Text style={styles.text}>Phone Number: {user.phoneNumber}</Text>
+        </>
+      ) : (
+        <Text style={styles.text}>Loading user data...</Text>
+      )}
+    </ScrollView>
+  );
 };
 
 const TreasurerScreen = () => {
-    return (
-        <Drawer.Navigator initialRouteName="Profile" screenOptions={{ headerShown: true }}>
-            <Drawer.Screen name="Profile" component={Profile} />
-            <Drawer.Screen name="IncomeStatement" component={IncomeStatement} />
-            <Drawer.Screen name="UnpaidDebts" component={UnpaidDebts} />
-            <Drawer.Screen name="AccountPayables" component={AccountPayables} />
-            <Drawer.Screen name="CoachManagement" component={CoachManagement} />
-            <Drawer.Screen name="MemberManagement" component={MemberManagement} />
-        </Drawer.Navigator>
-    );
+  return (
+    <Drawer.Navigator
+      initialRouteName="Profile"
+      screenOptions={{ headerShown: true }}
+    >
+      <Drawer.Screen name="Profile" component={Profile} />
+      <Drawer.Screen name="IncomeStatement" component={IncomeStatement} />
+      <Drawer.Screen name="UnpaidDebts" component={UnpaidDebts} />
+      <Drawer.Screen name="AccountPayables" component={AccountPayables} />
+      <Drawer.Screen name="CoachManagement" component={CoachManagement} />
+      <Drawer.Screen name="MemberManagement" component={MemberManagement} />
+    </Drawer.Navigator>
+  );
 };
 
 export default TreasurerScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flexGrow: 1,
-        padding: 20,
-    },
-    heading: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 20,
-    },
-    text: {
-        fontSize: 18,
-        marginBottom: 10,
-    },
-    profit: {
-        color: 'green',
-    },
+  container: {
+    flexGrow: 1,
+    padding: 20,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  text: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  profit: {
+    color: "green",
+  },
 });
